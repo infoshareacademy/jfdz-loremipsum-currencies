ae2d483e72fe4f16261cf6cbfb9c974a56011842
Marcin Bogucki <mar.bogucki@gmail.com>
2016-08-09 08:26:57 +0200

    aktualizacja wersji językowej

--- js/lang.js
+++ js/lang.js
@@ -1,20 +1,41 @@
 var lang = {
+
     pl: {
-        navigation1: 'Start',
-        navigation2: 'Funkcje',
-        navigation3: 'Kontakt',
-        navigation4: 'O nas',
-        navigation5: 'Mapa strony',
+        navigation_1: 'Start',
+        navigation_2: 'Funkcje',
+        navigation_3: 'Kontakt',
+        navigation_4: 'O nas',
+        navigation_5: 'Mapa strony',
 
-        btnContrast: 'Zmień kontrast'
+        btnContrast: 'Zmień kontrast',
+
+        form_heading: 'Kontakt do nas!',
+        form_email: 'Podaj adres e-mail:',
+        form_name: 'Twoje imię:',
+        form_msg: 'Wiadomość:',
+        form_btn: 'Wyślij',
+
+        about_heading: 'To my!',
+        it_is: 'To jest'
     },
-    en: {
-        navigation1: 'Home',
-        navigation2: 'Function',
-        navigation3: 'Contact',
-        navigation4: 'About us',
-        navigation5: 'Site Map',
 
-        btnContrast: 'change contrast'
+    en: {
+        navigation_1: 'Home',
+        navigation_2: 'Function',
+        navigation_3: 'Contact',
+        navigation_4: 'About us',
+        navigation_5: 'Site Map',
+
+        btnContrast: 'change contrast',
+
+        form_heading: 'Contact with Us!',
+        form_email: 'Give your e-mail address:',
+        form_name: 'Your firstname:',
+        form_msg: 'Message:',
+        form_btn: 'Send',
+
+        about_heading: 'Our Team!',
+        it_is: 'This is'
     }
+
 };
